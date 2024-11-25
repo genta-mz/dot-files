41f443702db8a46a86ad21c96d05feed32e4f0db
genta-mz <code.mizumoto@gmail.com>
2024-11-25 14:07:08 +0900

    fix

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,12 @@
+
+if ($Env:OS -eq "Windows_NT") {
+    $Env:Path += ";$HOME\AppData\Local\Programs\oh-my-posh\bin"
+}
+else {
+    $Env:PATH += ":$HOME/bin"
+}
+
+
 oh-my-posh init pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
 
 Set-PSReadLineKeyHandler -Chord Ctrl+r -ScriptBlock {
@@ -13,3 +22,12 @@ Set-PSReadLineKeyHandler -Chord Ctrl+r -ScriptBlock {
         [Microsoft.PowerShell.PSConsoleReadLine]::Insert($(echo $list | Out-GridView -PassThru))
     }
 }
+
+# PSReadLine有効化
+Import-Module PSReadLine
+
+# fish風のオートサジェスト機能を有効に
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle InlineView
+
+Set-PSReadLineKeyHandler -Key "Ctrl+f" -Function ForwardWord
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
